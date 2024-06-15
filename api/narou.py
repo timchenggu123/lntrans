72aea82753a89c1e2f63d69bbbe957b6dbc15c1c
Tim Gu <2013tim.g@gmail.com>
2024-06-15 12:22:02 -0400

    Updated API handler

--- api/narou.py
+++ api/narou.py
@@ -6,8 +6,23 @@ def get_novels_ranked(top=100) -> pd.DataFrame:
     '''
     Get the top novels from Narou
     Returns a DataFrame with the top novels' meta data
+
+    Parameters:
+    top (int): The number of novels to get. Value must be between 1 and 100
     '''
     response = requests.get(URL, params={"out": "json", "lim": top, "order": "hyoka"})
+    response = response.json()[1:]
+    df = pd.json_normalize(response)
+    df["updated_at"] = pd.to_datetime(df["updated_at"])
+    df["novelupdated_at"] = pd.to_datetime(df["novelupdated_at"])
+    return df
+
+def get_novels_newest(top=100) -> pd.DataFrame:
+    '''
+    Get the newest novels from Narou
+    Returns a DataFrame with the newest novels' meta data
+    '''
+    response = requests.get(URL, params={"out": "json", "lim": top, "order": "new"})
     response = response.json()[1:]
     df = pd.json_normalize(response)
     df["updated_at"] = pd.to_datetime(df["updated_at"])
