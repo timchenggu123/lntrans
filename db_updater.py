7598a8438a25715ffdcbe843794eba92b4bb1c8d
Tim Gu <2013tim.g@gmail.com>
2024-06-15 12:21:29 -0400

    Bether method for updating narou metadata.

--- db_updater.py
+++ db_updater.py
@@ -4,7 +4,7 @@ from api import narou as api
 def update_top_100_novels():
     # Connect to DB
     db = NarouDB()
-
+    db.connect()
     # Update the metadata
     # TODO: implement a better way to check if the data is updated
     df = api.get_novels_ranked(top=100)
@@ -13,7 +13,8 @@ def update_top_100_novels():
 def update_newest_novels():
     # Connect to DB
     db = NarouDB()
-
     df = api.get_novels_newest(top=100)
     db.update_novels_meta_raw(df)
     
+if __name__ == "__main__":
+    update_top_100_novels()
--- dl_narou.py
+++ dl_narou.py
@@ -1,31 +1,24 @@
-from crawlers import NcodeCrawler
+from crawlers.ncode import NcodeCrawler
 from api import narou
 from data import NarouDB
+from db_updater import update_top_100_novels
 import os
 
 def main():
     # Connect to DB
     db = NarouDB()
-    conn, cur = db.connect()
+    db.connect()
 
-    # Update the metadata
-    # TODO: implement a better way to check if the data is updated
-    do_update = False
-    if do_update:
-        df = narou.get_novels_ranked(top=10)
-        df.to_sql("novels_raw", conn, if_exists="replace")
-
-    cur.execute("SELECT ncode FROM novels_raw")
-    res = cur.fetchall()
-    
-    # Download the novels
-    output_dir = "raw/"
+    # Make the output directory if doesn't exists
+    output_dir = "raw/novels"
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
-    ncode_list = [res[i][0] for i in range(len(res))]
+
+    # Get the novel codes
+    ncode_list = db.get_ncodes(lim=1)  
     crawler = NcodeCrawler(output_dir=output_dir)
     for ncode in ncode_list:
-        crawler.queue_novel(ncode, s_chptr=1, e_chptr=1)
+        crawler.add(ncode)
     crawler.run()
 
 if __name__ == "__main__":
